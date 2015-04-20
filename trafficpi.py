863c3ec53b1c0d5135decf49eb8f8b4d8a6a998b
Ricky Moorhouse <ricky@samespirit.net>
2015-04-20 14:57:54 -0400

    refactoring into a single script

--- trafficpi.py
+++ trafficpi.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
-import xml.etree.ElementTree as ET
-import urllib2
+import dftTraffic
 import argparse
 
-def glow(colour):
-	f = open('/dev/ledborg','w')
-	f.write(colour)
-
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
 # Settings
 trafficfeed = "http://hatrafficinfo.dft.gov.uk/feeds/datex/England/JourneyTimeData/content.xml"
@@ -20,79 +19,53 @@ trafficfeed = "http://hatrafficinfo.dft.gov.uk/feeds/datex/England/JourneyTimeDa
 # Parse Parameters
 parser = argparse.ArgumentParser(description='Traffic indicator using LedBorg - Green, Amber or Red based on journey time.')
 parser.add_argument('--location',default="10710",help='specify the location code to check default: 10710 (M275 to M3)')
-parser.add_argument('--warn',default=25,help='Specify the minimum journey time to warn on default: 25')
-parser.add_argument('--alert',default=30,help='Specify the minimum journey time to alert on default: 30')
+parser.add_argument('--warn',default=1500,help='Specify the minimum journey time to warn on default: 25')
+parser.add_argument('--alert',default=1800,help='Specify the minimum journey time to alert on default: 30')
+parser.add_argument('--notify',default='console', help='How to notify - mqtt, piglow or console')
+
 args = parser.parse_args()
 if args.location:
 	selected_location = args.location
 
-''' Sample XML
-<d2LogicalModel modelBaseVersion="1.0">
-	<exchange>
-	...
-	</exchange>
-	<payloadPublication xsi:type="ElaboratedDataPublication" lang="en">
-	...
-
-		<elaboratedData id="GUID-2234086-10921">
-			<sourceInformation>
-				<sourceCountry>gb</sourceCountry>
-				<sourceIdentification>10921</sourceIdentification>
-				<sourceName><value lang="en">NTCC</value></sourceName>
-			</sourceInformation>
-			<basicDataValue xsi:type="TravelTimeValue">
-				<time>2013-02-03T15:15:00Z</time>
-				<affectedLocation>
-					<locationContainedInGroup xsi:type="LocationByReference">
-						<predefinedLocationReference>Section10921</predefinedLocationReference>
-					</locationContainedInGroup>
-					</affectedLocation>
-				<travelTime>13.0</travelTime>
-				<freeFlowTravelTime>14.0</freeFlowTravelTime>
-				<normallyExpectedTravelTime>14.0</normallyExpectedTravelTime>
-			</basicDataValue>
-		</elaboratedData>
-	</payloadPublication>
-</d2LogicalModel>
-'''
 
 
 
 # Get the feed over http
-traffic_xml=urllib2.urlopen(trafficfeed)
+times = dftTraffic.journey_times(args.location)
 
-# Parse the XML
-tree = ET.parse(traffic_xml)
-root = tree.getroot()
-
-# Dictionary to store the data for the location we're interested in
-times = {}
-
-# Loop through the elaboratedData sections
-for elab in root[1]:
-	# If it has an id and the id contains our location 
-	if (elab.attrib.get('id')) and (args.location in elab.attrib.get('id')):
-		# Load each item into our dictionary
-		for tagdata in elab[1]:
-			if "Time" in tagdata.tag:
-				times[tagdata.tag.replace('{http://datex2.eu/schema/1_0/1_0}','')] = tagdata.text
-
-#print times
-
-# Calculate delay from normally expected time (seconds) not currently used
-delay = float(times.get('travelTime')) - float(times.get('normallyExpectedTravelTime'))
 
 # Current travel time in minutes
-travelTime=float(times.get('travelTime')) / 60
-
-
+travelTime=times['current']
+if travelTime < 0:
+	travelTime = times['expected']
 
 if travelTime < args.warn:
-	# If it's lower than this let's glow green 
-	glow("020")
+	# If it's lower than this let's glow green
+	colour = "00ff00" 
 elif travelTime < args.alert:
 	# Between amber and red - must be amber
-	glow("120")
+	colour = "996600"
 else:
 	# Glow red
-	glow("200")
+	colour = "ff0000"
+
+if args.notify == 'mqtt':
+	import paho.mqtt.client as paho
+	# Connect to mqtt server
+	mqttc = paho.Client()
+	mqttc.connect("localhost", 1883, 60)
+	mqttc.publish("traffic/%s" % args.location, travelTime,qos=0,retain=True)
+	mqttc.publish("traffic", travelTime,qos=0,retain=True)
+	mqttc.publish("light/rgb", colour,qos=0,retain=True)
+elif args.notify == 'piglow':
+	from PyGlow import PyGlow
+	(r,g,b) = hex_to_rgb(colour)
+	PyGlow().led(1,r)
+	PyGlow().led(4,g)
+	PyGlow().led(5,b)
+else:
+	print "expected:  %s" % times['expected']
+	print "current:   %s" % times['current']
+	print "delay:     %f" % times['delay']
+	print "colour:    %s" % colour
+
