667674e1f315f5327641ebcf19575c9e0c66bde9
Ricky Moorhouse <ricky@samespirit.net>
2015-04-22 19:02:47 +0000

    style improvements

---
+++ DftTraffic.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+"""
+Module to parse the traffic information provided by the Department for Transport
+
+Currently supported:
+ - Journey time
+"""
+import xml.etree.ElementTree as ET
+import urllib2
+
+# Settings
+TRAFFIC_URL = "http://hatrafficinfo.dft.gov.uk/feeds/datex/England/JourneyTimeData/content.xml"
+
+def journey_times(route_id):
+    """Return the journey time for route (identified by route_id) in seconds"""
+    # Get the feed over http
+    traffic_xml = urllib2.urlopen(TRAFFIC_URL)
+ 
+    # Parse the XML
+    tree = ET.parse(traffic_xml)
+    root = tree.getroot()
+
+    # Dictionary to store the data for the location we're interested in
+    times = {}
+
+    # Loop through the elaboratedData sections
+    for elab in root[1]:
+        # If it has an id and the id contains our route_id
+        if (elab.attrib.get('id')) and (route_id in elab.attrib.get('id')):
+            # Load each item into our dictionary
+            for tagdata in elab[1]:
+                if "Time" in tagdata.tag:
+                    times[tagdata.tag.replace('{http://datex2.eu/schema/1_0/1_0}', '')] = tagdata.text
+                    # Calculate delay from normally expected time (seconds)
+                    if times.get('travelTime') < 0:
+                        delay = 0
+                    else:
+                        delay = float(times.get('travelTime')) - float(times.get('normallyExpectedTravelTime'))
+    return {
+        "expected":times.get('normallyExpectedTravelTime'),
+        "current":times.get('travelTime'),
+        "delay":delay
+    }
--- trafficpi.py
+++ trafficpi.py
@@ -1,71 +1,69 @@
 #!/usr/bin/python
-import dftTraffic
+import DftTraffic
 import argparse
 
 def hex_to_rgb(value):
-	value = value.lstrip('#')
-	lv = len(value)
-	return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
-# Settings
-trafficfeed = "http://hatrafficinfo.dft.gov.uk/feeds/datex/England/JourneyTimeData/content.xml"
 
-# Locations:	10501 - M3 to M275 along the M27 Eastbound
-#							10710 - M275 to M3 along the M27 Westbound
+# Locations:    10501 - M3 to M275 along the M27 Eastbound
+#    						10710 - M275 to M3 along the M27 Westbound
 
 # TODO: Add support for combined routes, where the journey is split in the source data
 # TODO: Change the defaults to be based on percentage of clear journey time or expected journey time
 
 # Parse Parameters
 parser = argparse.ArgumentParser(description='Traffic indicator using LedBorg - Green, Amber or Red based on journey time.')
-parser.add_argument('--location',default="10710",help='specify the location code to check default: 10710 (M275 to M3)')
-parser.add_argument('--warn',default=1500,help='Specify the minimum journey time to warn on default: 25')
-parser.add_argument('--alert',default=1800,help='Specify the minimum journey time to alert on default: 30')
-parser.add_argument('--notify',default='console', help='How to notify - mqtt, piglow or console')
+parser.add_argument('--location', default="10710", help='specify the location code to check default: 10710 (M275 to M3)')
+parser.add_argument('--warn', default=1500, help='Specify the minimum journey time to warn on default: 25')
+parser.add_argument('--alert', default=1800, help='Specify the minimum journey time to alert on default: 30')
+parser.add_argument('--notify', default='console', help='How to notify - mqtt,  piglow or console')
 
 args = parser.parse_args()
 if args.location:
-	selected_location = args.location
+    selected_location = args.location
 
 
 
 
 # Get the feed over http
-times = dftTraffic.journey_times(args.location)
+times = DftTraffic.journey_times(args.location)
 
 
 # Current travel time in minutes
-travelTime=times['current']
+travelTime = times['current']
 if travelTime < 0:
-	travelTime = times['expected']
+    travelTime = times['expected']
 
 if travelTime < args.warn:
-	# If it's lower than this let's glow green
-	colour = "00ff00" 
+    # If it's lower than this let's glow green
+    colour = "00ff00" 
 elif travelTime < args.alert:
-	# Between amber and red - must be amber
-	colour = "996600"
+    # Between amber and red - must be amber
+    colour = "996600"
 else:
-	# Glow red
-	colour = "ff0000"
+    # Glow red
+    colour = "ff0000"
 
 if args.notify == 'mqtt':
-	import paho.mqtt.client as paho
-	# Connect to mqtt server
-	mqttc = paho.Client()
-	mqttc.connect("localhost", 1883, 60)
-	mqttc.publish("traffic/%s" % args.location, travelTime,qos=0,retain=True)
-	mqttc.publish("traffic", travelTime,qos=0,retain=True)
-	mqttc.publish("light/rgb", colour,qos=0,retain=True)
+    import paho.mqtt.client as paho
+    # Connect to mqtt server
+    mqttc = paho.Client()
+    mqttc.connect("localhost", 1883, 60)
+    mqttc.publish("traffic/%s" % args.location, travelTime, qos=0, retain=True)
+    mqttc.publish("traffic", travelTime, qos=0, retain=True)
+    mqttc.publish("light/rgb", colour, qos=0, retain=True)
 elif args.notify == 'piglow':
-	from PyGlow import PyGlow
-	(r,g,b) = hex_to_rgb(colour)
-	PyGlow().led(1,r)
-	PyGlow().led(4,g)
-	PyGlow().led(5,b)
+    from PyGlow import PyGlow
+    (r, g, b) = hex_to_rgb(colour)
+    PyGlow().led(1, r)
+    PyGlow().led(4, g)
+    PyGlow().led(5, b)
 else:
-	print "expected:  %s" % times['expected']
-	print "current:   %s" % times['current']
-	print "delay:     %f" % times['delay']
-	print "colour:    %s" % colour
+    print "expected:  %s" % times['expected']
+    print "current:   %s" % times['current']
+    print "delay:     %f" % times['delay']
+    print "colour:    %s" % colour
 
