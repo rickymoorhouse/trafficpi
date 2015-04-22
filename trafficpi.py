c8a4ff3c607c97137e26531e7f7a615f439d24e3
Ricky Moorhouse <ricky@samespirit.net>
2015-04-22 18:28:39 +0000

    switching spaces to tabs

--- trafficpi.py
+++ trafficpi.py
@@ -3,9 +3,9 @@ import dftTraffic
 import argparse
 
 def hex_to_rgb(value):
-    value = value.lstrip('#')
-    lv = len(value)
-    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+	value = value.lstrip('#')
+	lv = len(value)
+	return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
 # Settings
 trafficfeed = "http://hatrafficinfo.dft.gov.uk/feeds/datex/England/JourneyTimeData/content.xml"
