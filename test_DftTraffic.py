915e097c28296a67af43355ac4e9d6a65de3b25e
Ricky Moorhouse <ricky@samespirit.net>
2015-12-20 23:03:16 +0000

    first steps to some module tests

---
+++ test_DftTraffic.py
@@ -0,0 +1,15 @@
+import pytest
+import inspect
+
+def test_sections():
+    import DftTraffic
+    dft = DftTraffic.DftTraffic()
+    roads = dft.find_section("M27")
+    assert 'M27' in roads['10185']
+def test_time():
+    import DftTraffic
+    dft = DftTraffic.DftTraffic()
+    times = dft.journey_times("10185")
+    assert 'updatedAt' in times
+    assert 'description' in times
+    assert 'normallyExpectedTravelTime' in times
