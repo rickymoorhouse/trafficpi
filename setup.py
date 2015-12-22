cb687c52003e26df574d1a14375dd622224a8ff9
Ricky Moorhouse <ricky@samespirit.net>
2015-12-22 07:30:27 +0000

    trying tox for tests

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = "DftTraffic",
+    version = "2.0",
+    packages = find_packages('DftTraffic'),  # include all packages under src
+    package_dir = {'':'DftTraffic'},   # tell distutils packages are under src
+
+    package_data = {
+        # If any package contains *.txt files, include them:
+        #'': ['*.txt'],
+    }
+)
